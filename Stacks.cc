bf5b16429845f241a21f9fd7aa3e69302087885f
FranKrdona07 <00119123@uca.edu.sv>
2024-10-02 07:54:49 -0600

    feat: agregar funcion de calculadora de postfijos

--- Stacks.cc
+++ Stacks.cc
@@ -1,34 +1,27 @@
 #include <iostream>
 #include <stack>
-
-void PedirDatos(int &pila_numeros);
-int main(int argc, char *argv[]){
-  int pila_numeros;
-  PedirDatos(pila_numeros);
+int Calculadora(std::string expresion);
 
 
+int main() {
+  std::string expresion;
+  std::cout << "Introduce una expresion postfija: ";
+  std::getline(std::cin, expresion);
   return 0;
 }
-void PedirDatos(int &pila_numeros){
-  std::stack<char> pila;
-    char valor;
 
-    // Solicitando al usuario que ingrese valores para apilar
-    std::cout << "Ingrese los valores que desea operar (0 para terminar): " << std::endl;
-    do
-    {
-      std::stack<std::char> temp = valor;
-      std::cout << "Valor: ";
-        std::cin >> valor;
-        if (valor == 0) break;
-        pila.push(valor);  // Apilando el valor
+int Calculadora(std::string expresion) {
+  std::stack<int> pila_numeros;
 
-        if (valor == '+')
-        {
-          
-        }
-        
-    } while (valor !=0);
+  for (int i = 0; i < expresion.size();++i) {  
+    char c = expresion[i];
 
-  return;
+    if (c == ' ') continue;
+
+    if (c >= '0' && c <= '9') {
+      pila_numeros.push(c -'0');  
+    }
+  }
+  
+  return pila_numeros.top();
 }
